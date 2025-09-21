2f0f10c9866b0dcdf9c07370d6bd93133e8f4626
IKurone <xande.cavalcante.costa@gmail.com>
2025-09-21 18:00:38 -0300

    add input validation and clear screen functionality in chess game loop

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,9 @@ import boardgame.Position;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import chess.exception.ChessException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,13 +15,25 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-            ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+                ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+            }
+            catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+
         }
     }
 }
