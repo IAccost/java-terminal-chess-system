f1e0174f4eb420e0ea4c5583da470ef8f41bf9f7
IKurone <xande.cavalcante.costa@gmail.com>
2025-09-21 23:07:29 -0300

    add check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,6 +6,7 @@ import chess.ChessPosition;
 import chess.Color;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class UI {
@@ -89,22 +90,30 @@ public class UI {
         System.out.print(" ");
     }
 
-    public static void printMatch(ChessMatch chessMatch) {
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
         System.out.println();
+        printCapturedPieces(captured);
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 
-//        if (!chessMatch.getCheckMate()) {
-//            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-//            if (chessMatch.getCheck()) {
-//                System.out.println("CHECK!");
-//            }
-//        }
-//        else {
-//            System.out.println("CHECKMATE!");
-//            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
-//        }
+        if(chessMatch.getCheck()) {
+            System.out.println("CHECK!");
+        }
 
+    }
+
+    private static void printCapturedPieces(List<ChessPiece> captured) {
+        java.util.List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).toList();
+        java.util.List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).toList();
+        System.out.println("Captured pieces:");
+        System.out.print("White: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(java.util.Arrays.toString(white.toArray()));
+        System.out.print(ANSI_RESET);
+        System.out.print("Black: ");
+        System.out.print(ANSI_YELLOW);
+        System.out.println(java.util.Arrays.toString(black.toArray()));
+        System.out.print(ANSI_RESET);
     }
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -27,6 +27,10 @@ public abstract class ChessPiece extends Piece {
         return p != null && p.getColor() != color;
     }
 
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
+    }
+
     public void increaseMoveCount() {
         moveCount++;
     }
