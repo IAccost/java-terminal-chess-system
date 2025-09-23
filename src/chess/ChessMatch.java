ae6b2322c106b9f7028298d37b57b94338efd9a0
IKurone <xande.cavalcante.costa@gmail.com>
2025-09-22 22:04:39 -0300

    fix promotion logic to handle invalid piece types in ChessMatch

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,7 +6,6 @@ import boardgame.Position;
 import chess.exception.ChessException;
 import chess.pieces.*;
 
-import java.lang.annotation.Target;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -86,7 +85,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-            throw new IllegalArgumentException("Invalid piece type for promotion");
+            return promoted;
         }
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
